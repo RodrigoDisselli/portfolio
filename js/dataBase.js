8e93cde152f0894b1dbb446042c5cb984b07b96a
Rodrigo Almeida <35853479+RodrigoDisselli@users.noreply.github.com>
2020-02-27 17:17:17 -0300

    add projetos e cases + funções para os botões e ocultei o scroll até segundo momento

--- js/dataBase.js
+++ js/dataBase.js
@@ -1,8 +1,16 @@
+const projects = [
+    {
+        title: "Tab Manager - Chrome extension",
+        image: "https://mir-s3-cdn-cf.behance.net/projects/404/0e270391084753.Y3JvcCwxOTIwLDE1MDEsMCwyODU.png",
+        url: "https://www.behance.net/gallery/91084753/Tab-Manager-Chrome-extension",
+    }
+];
+
 const articles = [
     {
-        title: "A importância do “vazio” no UI Design",
-        image: "https://miro.medium.com/max/2400/1*qazmF4oC1lbSivwp_kh8Jw.png",
-        url: "https://medium.com/@disselli.rodrigo/a-import%C3%A2ncia-do-vazio-no-ui-design-b0172cdce7f6",
+        title: "Base do uso de contraste no UI Design",
+        image: "https://miro.medium.com/max/3840/1*bWZOCimE1M2wKTzG9R0i_g.png",
+        url: "https://medium.com/@disselli.rodrigo/base-do-uso-de-contraste-no-ui-design-b949a9c3f51f",
     },
     {
         title: "A importância do “vazio” no UI Design",
@@ -10,43 +18,14 @@ const articles = [
         url: "https://medium.com/@disselli.rodrigo/a-import%C3%A2ncia-do-vazio-no-ui-design-b0172cdce7f6",
     },
     {
-        title: "A importância do “vazio” no UI Design",
-        image: "https://miro.medium.com/max/2400/1*qazmF4oC1lbSivwp_kh8Jw.png",
-        url: "https://medium.com/@disselli.rodrigo/a-import%C3%A2ncia-do-vazio-no-ui-design-b0172cdce7f6",
+        title: "#002 Daily UI — Página para Cadastro de Cartão de Crédito",
+        image: "https://miro.medium.com/max/1865/1*fBqmGgoCPynx486ldPxfgw.png",
+        url: "https://medium.com/@disselli.rodrigo/002-daily-ui-p%C3%A1gina-para-cadastro-de-cart%C3%A3o-de-cr%C3%A9dito-d3a4639150c2",
     },
     {
-        title: "A importância do “vazio” no UI Design",
-        image: "https://miro.medium.com/max/2400/1*qazmF4oC1lbSivwp_kh8Jw.png",
-        url: "https://medium.com/@disselli.rodrigo/a-import%C3%A2ncia-do-vazio-no-ui-design-b0172cdce7f6",
+        title: "#001 Daily UI — Sign Up Page",
+        image: "https://miro.medium.com/max/2500/1*PFHxS4h1Mtufs8iNOrhteQ.png",
+        url: "https://medium.com/@disselli.rodrigo/001-daily-ui-sign-up-page-f37b30cca5cf",
     },
-    {
-        title: "A importância do “vazio” no UI Design",
-        image: "https://miro.medium.com/max/2400/1*qazmF4oC1lbSivwp_kh8Jw.png",
-        url: "https://medium.com/@disselli.rodrigo/a-import%C3%A2ncia-do-vazio-no-ui-design-b0172cdce7f6",
-    },
-    {
-        title: "A importância do “vazio” no UI Design",
-        image: "https://miro.medium.com/max/2400/1*qazmF4oC1lbSivwp_kh8Jw.png",
-        url: "https://medium.com/@disselli.rodrigo/a-import%C3%A2ncia-do-vazio-no-ui-design-b0172cdce7f6",
-    },
-    {
-        title: "A importância do “vazio” no UI Design",
-        image: "https://miro.medium.com/max/2400/1*qazmF4oC1lbSivwp_kh8Jw.png",
-        url: "https://medium.com/@disselli.rodrigo/a-import%C3%A2ncia-do-vazio-no-ui-design-b0172cdce7f6",
-    },
-    {
-        title: "A importância do “vazio” no UI Design",
-        image: "https://miro.medium.com/max/2400/1*qazmF4oC1lbSivwp_kh8Jw.png",
-        url: "https://medium.com/@disselli.rodrigo/a-import%C3%A2ncia-do-vazio-no-ui-design-b0172cdce7f6",
-    },
-    {
-        title: "A importância do “vazio” no UI Design",
-        image: "https://miro.medium.com/max/2400/1*qazmF4oC1lbSivwp_kh8Jw.png",
-        url: "https://medium.com/@disselli.rodrigo/a-import%C3%A2ncia-do-vazio-no-ui-design-b0172cdce7f6",
-    },
-    {
-        title: "A importância do “vazio” no UI Design",
-        image: "https://miro.medium.com/max/2400/1*qazmF4oC1lbSivwp_kh8Jw.png",
-        url: "https://medium.com/@disselli.rodrigo/a-import%C3%A2ncia-do-vazio-no-ui-design-b0172cdce7f6",
-    },
-]
+];
+
--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
+const projectList = document.getElementById("project-list");
 const articleList = document.getElementById("article-list");
 
-const renderArticle = articles.map( (object) => createArticleList(object.title, object.image, object.url));
+const renderProjects = projects.map( (object) => createProjectList(object.title, object.image, object.url));
+const renderArticles = articles.map( (object) => createArticleList(object.title, object.image, object.url));
+
+function createProjectList(title, image, url) {
+  var div = document.createElement('div');
+  div.classList.add("card-list-horizontal-item");
+  div.style.backgroundImage = `url(${image})`;
+  div.innerHTML = `
+    <a href="${url}" target="_blank" class="card-opacity"></a>
+    <h3 class="card-title">${title}</h3>
+    `
+  return projectList.appendChild(div);
+}
 
 function createArticleList(title, image, url) {
     var div = document.createElement('div');
@@ -11,4 +24,8 @@ function createArticleList(title, image, url) {
       <h3 class="card-title">${title}</h3>
       `
     return articleList.appendChild(div);
+}
+
+function goTo(div) {
+  document.getElementById(div).scrollIntoView({behavior: "smooth"})
 }
