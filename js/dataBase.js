e7416ed83c9cd3bc2ba99879407481a2e8ab7727
RodrigoDisselli <disselli.rodrigo@gmail.com>
2020-02-28 19:45:08 -0300

    alteração de URL do Medium

--- js/dataBase.js
+++ js/dataBase.js
@@ -10,27 +10,27 @@ const articles = [
     {
         title: "Meu primeiro site de portfólio (Inception alert)",
         image: "https://miro.medium.com/max/1366/1*XSZkXqHkA6xbkeuClCMP0Q.png",
-        url: "https://medium.com/@disselli.rodrigo/meu-primeiro-site-de-portf%C3%B3lio-ux-ui-design-6b4751689b36",
+        url: "https://medium.com/@rodrigoalmeida.design/meu-primeiro-site-de-portf%C3%B3lio-ux-ui-design-6b4751689b36",
     },
     {
         title: "Base do uso de contraste no UI Design",
         image: "https://miro.medium.com/max/3840/1*bWZOCimE1M2wKTzG9R0i_g.png",
-        url: "https://medium.com/@disselli.rodrigo/base-do-uso-de-contraste-no-ui-design-b949a9c3f51f",
+        url: "https://medium.com/@rodrigoalmeida.design/base-do-uso-de-contraste-no-ui-design-b949a9c3f51f",
     },
     {
         title: "A importância do “vazio” no UI Design",
         image: "https://miro.medium.com/max/2400/1*qazmF4oC1lbSivwp_kh8Jw.png",
-        url: "https://medium.com/@disselli.rodrigo/a-import%C3%A2ncia-do-vazio-no-ui-design-b0172cdce7f6",
+        url: "https://medium.com/@rodrigoalmeida.design/a-import%C3%A2ncia-do-vazio-no-ui-design-b0172cdce7f6",
     },
     {
         title: "#002 Daily UI — Página para Cadastro de Cartão de Crédito",
         image: "https://miro.medium.com/max/1865/1*fBqmGgoCPynx486ldPxfgw.png",
-        url: "https://medium.com/@disselli.rodrigo/002-daily-ui-p%C3%A1gina-para-cadastro-de-cart%C3%A3o-de-cr%C3%A9dito-d3a4639150c2",
+        url: "https://medium.com/@rodrigoalmeida.design/002-daily-ui-p%C3%A1gina-para-cadastro-de-cart%C3%A3o-de-cr%C3%A9dito-d3a4639150c2",
     },
     {
         title: "#001 Daily UI — Sign Up Page",
         image: "https://miro.medium.com/max/2500/1*PFHxS4h1Mtufs8iNOrhteQ.png",
-        url: "https://medium.com/@disselli.rodrigo/001-daily-ui-sign-up-page-f37b30cca5cf",
+        url: "https://medium.com/@rodrigoalmeida.design/001-daily-ui-sign-up-page-f37b30cca5cf",
     },
 ];
 
