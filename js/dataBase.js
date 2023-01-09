3ca67e821d8ef4cf9c6e6859891c4a6f988d2856
AandVoid <aand.void@gmail.com>
2023-01-09 13:10:21 -0300

    update language

--- js/dataBase.js
+++ js/dataBase.js
@@ -1,41 +1,157 @@
-const projects = [
-    {
-        title: "App para gestão de gastos pessoais",
-        image: "https://mir-s3-cdn-cf.behance.net/projects/404/22150b93823409.Y3JvcCwzMjY2LDI1NTUsMTYsMA.jpg",
-        url: "https://www.behance.net/gallery/93823409/Gestao-de-gastos-pessoais",
-    },
-    {
-        title: "Tab Manager - Chrome extension",
-        image: "https://mir-s3-cdn-cf.behance.net/projects/404/0e270391084753.Y3JvcCwxOTIwLDE1MDEsMCwyODU.png",
-        url: "https://www.behance.net/gallery/91084753/Tab-Manager-Chrome-extension",
-    }
-];
+const languagePtbr ={
 
-const articles = [
-    {
-        title: "Meu primeiro site de portfólio",
-        image: "https://miro.medium.com/max/1920/1*4LCbCBbYUrz0aPzJHHQTgQ.png",
-        url: "https://medium.com/@rodrigoalmeida.design/meu-primeiro-site-de-portf%C3%B3lio-ux-ui-design-6b4751689b36",
-    },
-    {
-        title: "Base do uso de contraste no UI Design",
-        image: "https://miro.medium.com/max/3840/1*bWZOCimE1M2wKTzG9R0i_g.png",
-        url: "https://medium.com/@rodrigoalmeida.design/base-do-uso-de-contraste-no-ui-design-b949a9c3f51f",
-    },
-    {
-        title: "A importância do “vazio” no UI Design",
-        image: "https://miro.medium.com/max/2400/1*qazmF4oC1lbSivwp_kh8Jw.png",
-        url: "https://medium.com/@rodrigoalmeida.design/a-import%C3%A2ncia-do-vazio-no-ui-design-b0172cdce7f6",
-    },
-    {
-        title: "#002 Daily UI — Página para Cadastro de Cartão de Crédito",
-        image: "https://miro.medium.com/max/1865/1*fBqmGgoCPynx486ldPxfgw.png",
-        url: "https://medium.com/@rodrigoalmeida.design/002-daily-ui-p%C3%A1gina-para-cadastro-de-cart%C3%A3o-de-cr%C3%A9dito-d3a4639150c2",
-    },
-    {
-        title: "#001 Daily UI — Sign Up Page",
-        image: "https://miro.medium.com/max/2500/1*PFHxS4h1Mtufs8iNOrhteQ.png",
-        url: "https://medium.com/@rodrigoalmeida.design/001-daily-ui-sign-up-page-f37b30cca5cf",
-    },
-];
+    headerName: "Rodrigo D. Almeida",
+    headerRole: "UI/UX DESIGNER - PLENO",
+    headerXp: "5 ANOS DE EXPERIÊNCIA DE TRABALHO",
+    headerLocation: "Campinas/SP - Brasil",
+    headerPortfolio: "MEU PORTFÓLIO",
+    headerContactBtn: "ENTRE EM CONTATO",
 
+    aboutTitle: "Sobre",
+    aboutText1: "Trabalho como UX/UI Designer desde 2018, durante minha carreira ja tive a oportunidade de trabalhar com empresas de dentro e fora do Brasil.",
+    aboutText2: "Já trabalhei na melhoria e manutenção de sistemas Web acessados por milhares de usuários diariamente, participei na criação de Design Systems, levantamento de requisitos, estudo de usabilidade (utilizando dados de uso e os analisando por meio de tabelas e gráficos, em busca de padrões e insights), também já fiz entrevistas com usuários e stakeholdes (conduzidas tanto em português quanto em inglês).",
+    aboutText3: "Tenho conhecimento sobre lógica de programação e funcionamento de sistemas, o que me garante uma agilidade maior em me comunicar com desenvolvedores e analistas.",
+    aboutToolsTitle: "Experiente com as Ferramentas",
+    aboutAdditionalKnowledgeTitle: "Conhecimentos Adicionais",
+    aboutEnglishTitle: "INGLÊS AVANÇADO",
+    aboutEnglishSubtitle: "Conversação e Leitura<br>Experiência com reuniões internacionais e viagens ao exterior",
+    aboutWebTitle: "DESENVOLVIMENTO WEB BÁSICO",
+    aboutWebSubtitle: "HTML5 | CSS3 | JavaScript",
+    aboutIllustrationTitle: "ILUSTRAÇÃO DIGITAL",
+    aboutIllustrationSubtitle: "Experiente com desenho de personagens e cenas",
+    aboutVideoTitle: "EDIÇÃO DE VIDEO",
+    aboutVideoSubtitle: "Conhecimento básico de edição com Adobe Premiere e After Effects",
+
+
+    xpTitle: "Experiência",
+    xpRole1Title: "UI/UX Designer Pleno",
+    xpRole1Date: "Janeiro/2021 - Atualmente",
+    xpRole2Title: "UI/UX Designer Júnior",
+    xpRole2Date: "Maio/2020 - Janeiro/2021",
+    xpRole3Title: "Product Designer (UI/UX)<br>Júnior",
+    xpRole3Date: "Novembro/2019 - Abril/2020",
+    xpRole4Title: "Product Designer (UI/UX)<br>Treinee/Estagiário",
+    xpRole4Date: "Julho/2018 - Novembro/2019",
+
+
+    formationTitle: "Formação",
+    formationCollegeTitle: "Bacharelado em Engenharia de Computação",
+    formationCollegeDate: "2016 - 2020",
+    formationCoursesTitle: "Cursos e Certificados",
+    formationCourses: [
+        {
+            title: "UI DESIGN PATTERNS: USABILIDADE EM INTERFACES MOBILE",
+            subtitle: "Alura"
+        },
+        {
+            title: "Bootstrap: criação de uma single-page responsiva",
+            subtitle: "Alura"
+        },
+        {
+            title: "Gestalt: aplicando princípios em um layout Photoshop",
+            subtitle: "Alura"
+        },
+        {
+            title: "UX WRITING: CONTEÚDO FOCADO NO USUÁRIO",
+            subtitle: "Alura"
+        },
+        {
+            title: "Design Thinking parte 1: Utilize uma metodologia de design para solucionar desafios",
+            subtitle: "Alura"
+        },
+        {
+            title: "UX Designer",
+            subtitle: "Alura"
+        },
+        {
+            title: "Principles + Practices for Great UI Design",
+            subtitle: "Udemy"
+        },
+        {
+            title: "Flexbox: Posicione elementos na tela",
+            subtitle: "Alura"
+        },
+    ]
+
+
+};
+
+
+
+const languageEn ={
+
+    headerName: "Rodrigo D. Almeida",
+    headerRole: "UI/UX DESIGNER - MID-LEVEL",
+    headerXp: "5 YEARS OF EXPERIENCE",
+    headerLocation: "Campinas, SP - Brazil",
+    headerPortfolio: "MY PORTFOLIO",
+    headerContactBtn: "CONTACT ME",
+
+    aboutTitle: "About",
+    aboutText1: "I've been working as a UX/UI Designer since 2018, during my career I've had the opportunity to work with companies from inside and outside Brazil.",
+    aboutText2: "I've worked on improving and maintaining Web systems accessed by thousands of users daily, I've participated in the creation of Design Systems, requirements gathering, usability studies (Data analysis through tables and graphs, in search of patterns and insights), I have also conducted interviews with users and stakeholders (in both Portuguese and English).",
+    aboutText3: "I have programming logic and systems operation knowledge, which guarantees me greater agility in communicating with developers and analysts.",
+    aboutToolsTitle: "Most Used Tools",
+    aboutAdditionalKnowledgeTitle: "Additional Knowledge",
+    aboutEnglishTitle: "ADVANCED ENGLISH",
+    aboutEnglishSubtitle: "CConversation and Reading<br>Experience with international meetings and working abroad",
+    aboutWebTitle: "BASIC WEB DEVELOPMENT",
+    aboutWebSubtitle: "HTML5 | CSS3 | JavaScript",
+    aboutIllustrationTitle: "DIGITAL ILLUSTRATION",
+    aboutIllustrationSubtitle: "Experience with illustration for products",
+    aboutVideoTitle: "VIDEO EDITING",
+    aboutVideoSubtitle: "Basic knowledge of Adobe Premiere and After Effects",
+
+
+    xpTitle: "Experience",
+    xpRole1Title: "UI/UX Designer Mid-Level",
+    xpRole1Date: "January, 2021 - Currently",
+    xpRole2Title: "UI/UX Designer Junior",
+    xpRole2Date: "May, 2020 - January, 2021",
+    xpRole3Title: "Product Designer (UI/UX)<br>Junior",
+    xpRole3Date: "November, 2019 - April, 2020",
+    xpRole4Title: "Product Designer (UI/UX)<br>Treinee/Intern",
+    xpRole4Date: "July, 2018 - November, 2019",
+
+
+    formationTitle: "Graduation",
+    formationCollegeTitle: "Computer Engineer",
+    formationCollegeDate: "2016 - 2020",
+    formationCoursesTitle: "Courses and Certificates",
+    formationCourses: [
+        {
+            title: "UI DESIGN PATTERNS: USABILITY ON MOBILE INTERFACES",
+            subtitle: "Alura"
+        },
+        {
+            title: "Bootstrap: single-page creation",
+            subtitle: "Alura"
+        },
+        {
+            title: "Gestalt: applying principles of layout with Photoshop",
+            subtitle: "Alura"
+        },
+        {
+            title: "UX WRITING: CONTENT FOCUSED ON THE USER",
+            subtitle: "Alura"
+        },
+        {
+            title: "Design Thinking",
+            subtitle: "Alura"
+        },
+        {
+            title: "UX Designer",
+            subtitle: "Alura"
+        },
+        {
+            title: "Principles + Practices for Great UI Design",
+            subtitle: "Udemy"
+        },
+        {
+            title: "Flexbox: Place elements on the page",
+            subtitle: "Alura"
+        },
+    ]
+
+
+};
--- js/index.js
+++ js/index.js
@@ -1,50 +1,91 @@
-const projectList = document.getElementById("project-list");
-const articleList = document.getElementById("article-list");
 
-const renderProjects = projects.map( (object) => createList(object.title, object.image, object.url, projectList));
-const renderArticles = articles.map( (object) => createList(object.title, object.image, object.url, articleList));
+var userLanguage = window.navigator.userLanguage || window.navigator.language;
 
-function createList(title, image, url, parent) {
-  var div = document.createElement('div');
-  div.classList.add("card-list-horizontal-item");
-  div.style.backgroundImage = `url(${image})`;
-  div.innerHTML = `
-    <div onclick="window.open('${url}', '_blank');" class="card-opacity"></div>
-    <div onclick="window.open('${url}', '_blank');" class="card-title">${title}</div>
-    `
-  return parent.appendChild(div);
+// console.log(userLanguage);
+
+// Start
+window.onload = () => populatePage(userLanguage);
+
+
+function accessLink(url) {
+    window.open(url, '_blank').focus();
 }
 
-function goTo(div) {
-  document.getElementById(div).scrollIntoView({behavior: "smooth"})
+function populatePage(userLanguage){
+
+    // console.log(userLanguage)
+
+    if (userLanguage != 'pt-BR') {
+        printData(languageEn)
+        return languageEn
+    }else {
+        printData(languagePtbr)  
+        return languagePtbr
+    }
+    
 }
 
-function copyText(text, label, elementId){
-  var target = document.getElementById(elementId);
-  var copyAlert = document.createElement('div');
-  copyAlert.classList.add("inline-alert");
-  copyAlert.innerHTML=`
-    ${label}
-  `
+function printData(language){
 
 
-  const el = document.createElement('textarea');
-  el.value = text;
-  document.body.appendChild(el);
-  el.select();
-  document.execCommand('copy');
-  document.body.removeChild(el);
+  document.getElementById("header-name").innerHTML=language.headerName;
+  document.getElementById("header-role").innerHTML=language.headerRole;
+  document.getElementById("header-xp").innerHTML=language.headerXp;
+  document.getElementById("header-location").innerHTML=language.headerLocation;
+  document.getElementById("header-portfolio").innerHTML=language.headerPortfolio;
+  document.getElementById("header-contact-btn").innerHTML=language.headerContactBtn;
+
+  document.getElementById("about-title").innerHTML=language.aboutTitle;
+  document.getElementById("about-text1").innerHTML=language.aboutText1;
+  document.getElementById("about-text2").innerHTML=language.aboutText2;
+  document.getElementById("about-text3").innerHTML=language.aboutText3;
+  document.getElementById("about-tools-title").innerHTML=language.aboutToolsTitle;
+  document.getElementById("about-additional-knowledge-title").innerHTML=language.aboutAdditionalKnowledgeTitle;
+  document.getElementById("about-english-title").innerHTML=language.aboutEnglishTitle;
+  document.getElementById("about-english-subtitle").innerHTML=language.aboutEnglishSubtitle;
+  document.getElementById("about-web-title").innerHTML=language.aboutWebTitle;
+  document.getElementById("about-web-subtitle").innerHTML=language.aboutWebSubtitle;
+  document.getElementById("about-illustration-title").innerHTML=language.aboutIllustrationTitle;
+  document.getElementById("about-illustration-subtitle").innerHTML=language.aboutIllustrationSubtitle;
+  document.getElementById("about-video-title").innerHTML=language.aboutVideoTitle;
+  document.getElementById("about-video-subtitle").innerHTML=language.aboutVideoSubtitle;
+
+  document.getElementById("xp-title").innerHTML=language.xpTitle;
+  document.getElementById("xp-role1-title").innerHTML=language.xpRole1Title;
+  document.getElementById("xp-role1-date").innerHTML=language.xpRole1Date;
+  document.getElementById("xp-role2-title").innerHTML=language.xpRole2Title;
+  document.getElementById("xp-role2-date").innerHTML=language.xpRole2Date;
+  document.getElementById("xp-role3-title").innerHTML=language.xpRole3Title;
+  document.getElementById("xp-role3-date").innerHTML=language.xpRole3Date;
+  document.getElementById("xp-role4-title").innerHTML=language.xpRole4Title;
+  document.getElementById("xp-role4-date").innerHTML=language.xpRole4Date;
+
+  document.getElementById("xp-title-mobile").innerHTML=language.xpTitle;
+  document.getElementById("xp-role1-title-mobile").innerHTML=language.xpRole1Title;
+  document.getElementById("xp-role1-date-mobile").innerHTML=language.xpRole1Date;
+  document.getElementById("xp-role2-title-mobile").innerHTML=language.xpRole2Title;
+  document.getElementById("xp-role2-date-mobile").innerHTML=language.xpRole2Date;
+  document.getElementById("xp-role3-title-mobile").innerHTML=language.xpRole3Title;
+  document.getElementById("xp-role3-date-mobile").innerHTML=language.xpRole3Date;
+  document.getElementById("xp-role4-title-mobile").innerHTML=language.xpRole4Title;
+  document.getElementById("xp-role4-date-mobile").innerHTML=language.xpRole4Date;
+
+  document.getElementById("formation-title").innerHTML=language.formationTitle;
+  document.getElementById("formation-college-title").innerHTML=language.formationCollegeTitle;
+  document.getElementById("formation-college-date").innerHTML=language.formationCollegeDate;
+  document.getElementById("formation-courses-title").innerHTML=language.formationCoursesTitle;
+
+  for (i=1; i <= language.formationCourses.length; i++) {
+    var titleId = "formation-courses-title" + i;
+    var subtitleId = "formation-courses-subtitle" + i;
 
 
-  if(target.offsetLeft > (window.innerWidth / 2)){
-    copyAlert.style.right = "0px";
-    copyAlert.style.left = "unset";
+    document.getElementById(titleId).innerHTML=language.formationCourses[i-1].title;
+    document.getElementById(subtitleId).innerHTML=language.formationCourses[i-1].subtitle;
   }
 
+  document.getElementById("footer-portfolio").innerHTML=language.headerPortfolio;
+  document.getElementById("footer-contact-btn").innerHTML=language.headerContactBtn;
 
-  target.style.position = "relative";
-  target.appendChild(copyAlert);
-  setTimeout(() => {
-    target.removeChild(copyAlert);
-  },"1500");
+
 }
